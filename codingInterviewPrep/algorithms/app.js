07d0583b88ef21c6bc47fec1129bef4b482ef34d
Toan <tpkhuong@gmail.com>
2021-03-23 06:46:01 -0400

    workin on permutations with unique

--- codingInterviewPrep/algorithms/app.js
+++ codingInterviewPrep/algorithms/app.js
@@ -1,3 +1,5 @@
+/***** Symmetric Difference *****/
+
 /*
 
 Find the Symmetric Difference
@@ -107,6 +109,10 @@ function notes() {
 /***** recursion *****/
 
 /***** recursion *****/
+
+/***** Symmetric Difference *****/
+
+/***** Inventory Update *****/
 
 /*
 
@@ -242,3 +248,20 @@ function updateInventoryUsingObj(currentInv, newInv) {
 }
 
 /***** difference approach to updateInventory without nested loops *****/
+
+/***** Inventory Update *****/
+
+/***** No Repeats Please *****/
+
+/*
+
+No Repeats Please
+Return the number of total permutations of the provided string that don't have repeated consecutive letters. Assume that all characters in the provided string are each unique.
+
+For example, aab should return 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating.
+
+*/
+
+function singlePermutation(strInput) {}
+
+/***** No Repeats Please *****/
