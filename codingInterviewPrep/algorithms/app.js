a2e1f99b67dce749584443d41b33d680006d7991
Toan <tpkhuong@gmail.com>
2021-04-04 07:28:09 -0400

    working on pairwise challenge

--- codingInterviewPrep/algorithms/app.js
+++ codingInterviewPrep/algorithms/app.js
@@ -397,3 +397,75 @@ var thirdArr = [6, 7, 8, 9];
 
 restul = [...result, [...thirdArr]];
 /***** No Repeats Please *****/
+
+/***** Pairwise *****/
+/*
+Pairwise
+Given an array arr, find element pairs whose sum equal the second argument arg and return the sum of their indices.
+
+You may use multiple pairs that have the same numeric elements but different indices. Each pair should use the lowest possible available indices.
+Once an element has been used it cannot be reused to pair with another element.
+For instance, pairwise([1, 1, 2], 3) creates a pair [2, 1] using the 1 at index 0 rather than the 1 at index 1, because 0+2 < 1+2.
+
+For example pairwise([7, 9, 11, 13, 15], 20) returns 6. The pairs that sum to 20 are [7, 13] and [9, 11]. We can then write out the array with their indices and values.
+
+Index	0	1	2	3	4
+Value	7	9	11	13	15
+
+Below we'll take their corresponding indices and add them.
+
+7 + 13 = 20 → Indices 0 + 3 = 3
+9 + 11 = 20 → Indices 1 + 2 = 3
+3 + 3 = 6 → Return 6
+
+pairwise([0, 0, 0, 0, 1, 1], 1)
+
+*/
+
+var storeInnerFunc;
+
+function pairwise(arrInput, sumInput) {
+  //for repeat value keep track of the lowest index
+  //want to use an obj when adding two pairs give us the total of sumInput save the value and its index in the original array.
+  //keep track of repeated value
+  //using reduce
+  var visited = {};
+  function helperFunc() {
+    var arrOfObj = arrInput.reduce(function findPairEqualToSum(
+      buildingUp,
+      currentValue,
+      currIndex,
+      list
+    ) {
+      var lastIndex = list.length - 1;
+      if (currIndex == lastIndex) {
+        return buildingUp;
+      } else {
+        if (!visited[currentValue]) {
+          for (let i = currIndex + 1; i < list.length; i++) {
+            let ourCalcOfTwoPairNums = currentValue + list[i];
+            if (ourCalcOfTwoPairNums == sumInput) {
+              return [
+                ...building,
+                {
+                  firstIndex: currIndex,
+                  secondIndex: currIndex + 1,
+                  firstValue: currentValue,
+                  secondValue: list[i],
+                },
+              ];
+            }
+            return buildingUp;
+          }
+          visited[currentValue] = true;
+        } else {
+          return buildingUp;
+        }
+      }
+    },
+    []);
+  }
+
+  storeInnerFunc = helperFunc;
+}
+/***** Pairwise *****/
