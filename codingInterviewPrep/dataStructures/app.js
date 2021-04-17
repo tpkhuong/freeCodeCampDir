e88f3c0915ff331a236f860108edc98448de70fe
Toan <tpkhuong@gmail.com>
2021-04-17 06:43:44 -0400

    finished up toe queue challenge

--- codingInterviewPrep/dataStructures/app.js
+++ codingInterviewPrep/dataStructures/app.js
@@ -1,6 +1,6 @@
 /*
 
-Typed Arrays
+***** Typed Arrays *****
 Arrays are JavaScript objects that can hold a lot of different elements.
 
 var complexArr = [1, 5, "2", "Word", {"name": "James"}];
@@ -68,4 +68,134 @@ class Int32Array extends ArrayBuffer {
     }
     return this.resultArr;
   }
+}
+
+/*
+
+***** Stack *****
+
+Learn how a Stack Works
+You are probably familiar with stack of books on your table. You have likely used the undo feature of a text editor.
+You are also probably used to hitting the back button on your phone to go back to the previous view in your app.
+
+You know what they all have in common? They all store the data in a way so that you can traverse backwards.
+
+The topmost book in the stack was the one that was put there last. If you remove that book from your stack's top,
+you would expose the book that was put there before the last book and so on.
+
+If you think about it, in all the above examples, you are getting Last-In-First-Out type of service. We will try to mimic this with our code.
+
+This data storage scheme is called a Stack. In particular, we would have to implement the push() method that pushes
+JavaScript objects at the top of the stack; and pop() method, that removes the JavaScript object that's at the top of the stack at the current moment.
+
+Here we have a stack of homework assignments represented as an array: "BIO12" is at the base, and "PSY44" is at the top of the stack.
+
+Modify the given array and treat it like a stack using the JavaScript methods mentioned above.
+Remove the top element "PSY44" from the stack. Then add "CS50" to be the new top element of the stack.
+
+***** Stack *****
+
+*/
+
+class Stack {
+  constructor(arrInput) {
+    this.ourArray = arrInput;
+    this.copiedArr = [...arrInput];
+  }
+  push(valueInput) {
+    this.copiedArr.push(valueInput);
+  }
+  pop() {
+    var returnValue = this.copiedArr.pop();
+    return returnValue;
+  }
+  view() {
+    console.log(this.copiedArr);
+  }
+}
+
+var homeworkStack = ["BIO12", "HIS80", "MAT122", "PSY44"];
+
+var newStack = new Stack(homeworkStack);
+
+/*
+
+***** Queue *****
+
+Create a Queue Class
+Like stacks, queues are a collection of elements. But unlike stacks, queues follow the FIFO (First-In First-Out) principle.
+Elements added to a queue are pushed to the tail, or the end, of the queue, and only the element at the front of the queue is allowed to be removed.
+
+We could use an array to represent a queue, but just like stacks, we want to limit the amount of control we have over our queues.
+
+The two main methods of a queue class is the enqueue and the dequeue method. The enqueue method pushes an element to the tail of the queue,
+and the dequeue method removes and returns the element at the front of the queue. Other useful methods are the front, size, and isEmpty methods.
+
+Write an enqueue method that pushes an element to the tail of the queue, a dequeue method that removes and returns the front element,
+a front method that lets us see the front element, a size method that shows the length, and an isEmpty method to check if the queue is empty.
+
+***** Queue *****
+
+*/
+
+class Queue {
+  constructor() {
+    this.collection = [];
+  }
+  enqueue(valueInput) {
+    this.collection.push(valueInput);
+    return this.collection;
+  }
+  dequeue() {
+    var removedValue = this.collection.shift();
+    return removedValue;
+  }
+  front() {
+    var firstValue = this.collection[0];
+    return firstValue;
+  }
+  size() {
+    return this.collection.length;
+  }
+  isEmpty() {
+    return this.collection.length > 0 ? false : true;
+  }
+}
+
+/*
+
+***** Priority Queue *****
+
+Create a Priority Queue Class
+In this challenge you will be creating a Priority Queue. A Priority Queue is a special type of Queue in which items may have additional
+information which specifies their priority. This could be simply represented with an integer. Item priority will override placement order in determining the sequence items are dequeued.
+If an item with a higher priority is enqueued after items with lower priority, the higher priority item will be dequeued before all the others.
+
+For instance, let’s imagine we have a priority queue with three items:
+
+[['kitten', 2], ['dog', 2], ['rabbit', 2]]
+Here the second value (an integer) represents item priority. If we enqueue ['human', 1] with a priority of 1 (assuming lower priorities are given precedence)
+it would then be the first item to be dequeued. The collection would look like this:
+
+[['human', 1], ['kitten', 2], ['dog', 2], ['rabbit', 2]]
+We’ve started writing a PriorityQueue in the code editor. You will need to add an enqueue method for adding items with a priority, a dequeue method for removing and returning items,
+a size method to return the number of items in the queue, a front method to return the element at the front of the queue, and finally an isEmpty method that will return true if
+the queue is empty or false if it is not.
+
+The enqueue should accept items with the format shown above (['human', 1]) where 1 represents the priority. dequeue and front should return only the item's name, not its priority.
+
+
+***** Priority Queue *****
+
+*/
+
+class PriorityQueue {
+  constructor() {
+    this.collection = [];
+  }
+  enqueue() {}
+  dequeue() {}
+  size() {}
+  front() {}
+  isEmpty() {}
 }
