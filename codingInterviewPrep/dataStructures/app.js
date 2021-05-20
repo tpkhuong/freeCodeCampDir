cd48b66529ef4ad96129805dbba0f30cd6083f38
Toan <tpkhuong@gmail.com>
2021-05-20 06:30:59 -0400

    working on isSubsetOf Set Data structure

--- codingInterviewPrep/dataStructures/app.js
+++ codingInterviewPrep/dataStructures/app.js
@@ -834,6 +834,16 @@ Otherwise, it should return false. */
     }
     return -1;
   }
+
+  /*
+  
+  Perform a Subset Check on Two Sets of Data
+  In this exercise, we are going to perform a subset test on 2 sets of data. We will create a method on our Set data structure called isSubsetOf.
+  This will compare the first set against the second, and if the first set is fully contained within the second, it will return true.
+
+  For example, if setA = ['a','b'] and setB = ['a','b','c','d'], then setA is a subset of setB, so setA.isSubsetOf(setB) should return true.
+  
+  */
 }
 
 var testArr = [1, 2, 3, 4, 5, 6];
