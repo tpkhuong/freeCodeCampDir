2a15f902902ef816734e9992a80f241ab82f4641
Toan <tpkhuong@gmail.com>
2021-06-05 06:35:58 -0400

    working on Map Data structure

--- codingInterviewPrep/dataStructures/app.js
+++ codingInterviewPrep/dataStructures/app.js
@@ -1181,7 +1181,48 @@ var howBig = set.size;
   size() {
     return this.collection.length;
   }
+  /*
+
+  Data Structures
+  Use Spread and Notes for ES5 Set() Integration
+  Do you remember the ES6 spread operator ...?
+
+  ... can take iterable objects in ES6 and turn them into arrays.
+
+  Let's create a Set, and check out the spread function.
+
+  var set = new Set([1,2,3]);
+  var setToArr = [...set]
+  console.log(setToArr) // returns [ 1, 2, 3 ]
+  
+  */
+  spread(objInput) {
+    var copyOfObj = Object.assign({}, objInput);
+    var valuesOfObj = Object.values(copyOfObj);
+
+    return valuesOfObj;
+  }
 }
+
+/*
+
+Create a Map Data Structure
+The next few challenges will cover maps and hash tables. Maps are data structures that store key-value pairs. In JavaScript, these are available to us as objects.
+Maps provide rapid lookup of stored items based on key values and are very common and useful data structures.
+
+Let's get some practice creating our own map. Because JavaScript objects provide a much more efficient map structure than anything we could write here,
+this is intended primarily as a learning exercise. However, JavaScript objects only provide us with certain operations. What if we wanted to define custom operations?
+Use the Map object provided here as a wrapper around a JavaScript object. Create the following methods and operations on the Map object:
+
+add accepts a key, value pair to add to the map.
+remove accepts a key and removes the associated key, value pair
+get accepts a key and returns the stored value
+has accepts a key and returns true if the key exists or false if it doesn't.
+values returns an array of all the values in the map
+size returns the number of items in the map
+clear empties the map
+
+*/
 
 var arr = [1, 2, 3, 4, 5, 6];
 var index = 3;
